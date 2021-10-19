3c324acda36eab742b26eabf16f51f49caac9042
duckbok <duckbok22@gmail.com>
2021-10-20 02:55:30 +0900

    fix: Library Path 변경 적용
    Fixes: #4, #6

--- GameLog/Sources/Service/LibrearyService.swift
+++ GameLog/Sources/Service/LibrearyService.swift
@@ -9,7 +9,7 @@ struct LibraryService {
 
     typealias ServiceResult = Result<Library, NetworkRepository.Error>
 
-    private let getPath: String = "game/library/get/"
+    private let path: String = "game/library"
 
     private let networkRepository: NetworkRepository
 
@@ -21,7 +21,7 @@ struct LibraryService {
               sortingMethod: Library.SortingMethod,
               sortingOrder: Library.SortingOrder,
               completion: @escaping (ServiceResult) -> Void) {
-        networkRepository.get(path: getPath, query: ["page": page.description,
+        networkRepository.get(path: path, query: ["page": page.description,
                                                      "sort": sortingMethod.rawValue,
                                                      "sorttype": sortingOrder.rawValue]) { (result: ServiceResult) in
             completion(result)
