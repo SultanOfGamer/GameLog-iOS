b9c330905252f3d7f6be6770f902346591630ca6
duckbok <duckbok22@gmail.com>
2021-10-20 12:28:01 +0900

    feat: SearchViewModel과 Cell 구현
    Fixes: #4, #7

---
+++ GameLog/Sources/ViewModels/SearchViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  SearchViewModel.swift
+//  GameLog
+//
+//  Created by duckbok on 2021/10/20.
+//
+
+import Foundation
+
+final class SearchViewModel {
+
+    var dataSource: Global.SearchDataSource?
+
+    private let searchService = SearchService()
+
+    private var search: Search? {
+        didSet {
+            applySnapshot(search: search)
+        }
+    }
+
+    func fetchGames(by name: String?) {
+        guard let name = name else {
+            search = nil
+            return
+        }
+
+        searchService.search(by: name) { [weak self] result in
+            switch result {
+            case let .success(search):
+                self?.search = search
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+
+    func applySnapshot(search: Search?, animatingDifferences: Bool = true) {
+        var snapshot = Global.SearchSnapshot()
+        if let search = search {
+            snapshot.appendSections([search])
+            snapshot.appendItems(search.games, toSection: search)
+        }
+        dataSource?.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+}
